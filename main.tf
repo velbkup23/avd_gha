104d7e51f6a892427556c269e207aa8593ed8441
Vel user <veluser@juilletvelazoutlook.onmicrosoft.com>
2025-07-16 11:08:07 +0530

    IC5

--- main.tf
+++ main.tf
@@ -7,3 +7,8 @@ resource "azurerm_resource_group" "gha_rg" {
   name     = "gha-rg2"
   location = var.location
 }
+
+resource "azurerm_resource_group" "gha_rg3" {
+  name     = "gha-rg3"
+  location = var.location
+}
